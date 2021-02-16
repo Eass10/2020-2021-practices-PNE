2ccae7a693ab088a50816c9dc92a1c25887796a7
Eass <enriquealmazan0010@gmail.com>
2021-02-16 16:01:43 +0100

    Upgrades

--- Tutorial/count.py
+++ Tutorial/count.py
@@ -1,2 +1,3 @@
 for n in range(1,21):
+    print("The list of numbers is:")
     print(n, end=",")
--- Tutorial/sum20.py
+++ Tutorial/sum20.py
@@ -1,4 +1,4 @@
 sum = 0
 for n in range(1,21):
     sum += n
-print("The sum of the first 20 natural number is:", sum)
+print("The sum of the first 20 natural number:", sum)
