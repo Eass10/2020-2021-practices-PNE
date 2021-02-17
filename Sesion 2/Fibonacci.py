94a8fe13e766247b72f73d3cd4a3b16225a7c11f
Eass <enriquealmazan0010@gmail.com>
2021-02-17 18:25:55 +0100

    Upgrades

--- Sesion 2/Fibonacci.py
+++ Sesion 2/Fibonacci.py
@@ -2,18 +2,18 @@ def fibonacci(n):
     a = 0
     b = 1
     series = [a,b]
-    for i in range(1, n+1):
+    for i in range(1, n-1):
         c = a + b
         series.append(c)
         a = b
         b = c
-    return series
+    return series, c
 exit = False
 while not exit:
     try:
         n = int(input("Introduce a natural number: "))
         if n > 0:
-            print("The Fibonacci series for " + str(n) + " is: ", fibonacci(n))
+            print("The " + str(n) + " number of the Fibonacci series for is: " + str(fibonacci(n)[1]) + " ,and the Fibonacci series for that number is: ", fibonacci(n)[0])
             exit = True
         else:
             print("The number you have introduced is not valid. Please introduce a natural number")
